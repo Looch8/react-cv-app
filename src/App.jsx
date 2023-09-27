0607d5c4ddb4ced00a25368d8d8a97b25bfe018f
Luke Wheldale <lukewheldale@gmail.com>
2023-09-27 19:53:48 +0930

    feat: added one submit button for name, email, and phone number

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,22 @@ import styles from "./styles/styles.css";
 
 function App() {
 	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
 	const [experience, setExperience] = useState("");
 	const [education, setEducation] = useState("");
 
 	// Define function to update the name state in App.jsx
 	const updateName = (newName) => {
 		setName(newName);
+	};
+
+	const updateEmail = (newEmail) => {
+		setEmail(newEmail);
+	};
+
+	const updatePhone = (newPhone) => {
+		setPhone(newPhone);
 	};
 
 	const updateExperience = (newExperience) => {
@@ -22,30 +32,40 @@ function App() {
 		setEducation(newEducation);
 	};
 
+	const handleGeneralSubmit = (generalData) => {
+		updateName(generalData.name);
+		updateEmail(generalData.email);
+		updatePhone(generalData.phone);
+	};
+
 	return (
 		<>
-			<form>
-				<h3>Fill in the fields to generate your resume:</h3>
-				<section className="general">
-					<General value={name} updateName={updateName} />
-				</section>
-				<section className="experience">
-					<Experience
-						value={experience}
-						updateExperience={updateExperience}
-					/>
-				</section>
-				<section className="education">
-					<Education
-						value={education}
-						updateEducation={updateEducation}
-					/>
-				</section>
-			</form>
+			<h3>Fill in the fields to generate your resume:</h3>
+			<section className="general">
+				<General onSubmit={handleGeneralSubmit} />
+			</section>
+
+			<section className="experience">
+				<Experience
+					value={experience}
+					updateExperience={updateExperience}
+				/>
+			</section>
+			<section className="education">
+				<Education
+					value={education}
+					updateEducation={updateEducation}
+				/>
+			</section>
+
 			<section className="resume-container">
 				<h1>Resume</h1>
 				<label htmlFor="name">Name: </label>
 				<p>{name}</p>
+				<label htmlFor="email">Email: </label>
+				<p>{email}</p>
+				<label htmlFor="phone">Phone: </label>
+				<p>{phone}</p>
 				<label htmlFor="experience">Experience: </label>
 				<p>{experience}</p>
 				<label htmlFor="education">Education:</label>
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,33 +1,61 @@
 import { useState } from "react";
 
-function General({ value, updateName }) {
-	const [name, setName] = useState(value);
+function General({ onSubmit }) {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
 
 	const handleChange = (e) => {
-		setName(e.target.value);
+		if (e.target.name === "name") {
+			setName(e.target.value);
+		} else if (e.target.name === "email") {
+			setEmail(e.target.value);
+		} else if (e.target.name === "phone") {
+			setPhone(e.target.value);
+		}
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		// Call the updateName function to update name in App component
-		updateName(name);
+		onSubmit({ name, email, phone });
 		setName(""); // Makes the input field empty after submitting
+		setEmail(""); // Makes the input field empty after submitting
+		setPhone(""); // Makes the input field empty after submitting
 	};
 
 	return (
 		<>
-			<label htmlFor="name">Name: </label>
-			<input
-				type="text"
-				id="name"
-				name="name"
-				value={name}
-				onChange={handleChange}
-			/>
-			<button className="btn" type="submit" onClick={handleSubmit}>
-				{" "}
-				Submit{" "}
-			</button>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Name: </label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					value={name}
+					onChange={handleChange}
+				/>
+
+				<label htmlFor="email">Email: </label>
+				<input
+					type="text"
+					id="email"
+					name="email"
+					value={email}
+					onChange={handleChange}
+				/>
+
+				<label htmlFor="phone">Phone: </label>
+				<input
+					type="text"
+					id="phone"
+					name="phone"
+					value={phone}
+					onChange={handleChange}
+				/>
+				<button className="btn" type="submit">
+					Submit
+				</button>
+			</form>
 		</>
 	);
 }
